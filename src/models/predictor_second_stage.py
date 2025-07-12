fb9fead816fe783c8028daecb1a35a1029a39326
francescobraicovich <francesco.braicovich@gmail.com>
2025-07-12 02:30:17 +0200

    Fix import paths in predictor_second_stage.py

--- src/models/predictor_second_stage.py
+++ src/models/predictor_second_stage.py
@@ -9,9 +9,9 @@ from functools import partial
 import torch
 import torch.nn as nn
 
-from src.models.utils.modules import ACBlock as Block
-from src.models.utils.modules import build_action_block_causal_attention_mask
-from src.utils.fb_tensors import trunc_normal_
+from models.utils.modules import ACBlock as Block
+from models.utils.modules import build_action_block_causal_attention_mask
+from utils.fb_tensors import trunc_normal_
 
 
 class VisionTransformerPredictorAC(nn.Module):
